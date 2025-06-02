35377a3401272cdf00771614fb68448dc650ad4a
Paul Novack <paul.novack@gmail.com>
2025-06-01 19:36:27 -0500

    adds automatic server restart if config changes

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM ubuntu:25.04
 ENV DEBIAN_FRONTEND=noninteractive
 ENV TZ=Etc/UTC
 RUN apt-get update \
- && apt-get install -y tzdata nodejs npm \
+ && apt-get install -y tzdata nodejs npm lsof inotify-tools \
  && rm -rf /var/lib/apt/lists/*
 WORKDIR /app
 COPY package*.json ./
--- startMockoon.sh
+++ startMockoon.sh
@@ -1,3 +1,19 @@
-kill -9 $(lsof -ti :3000)
-NODE_OPTIONS="--require $(pwd)/mockoon-hbs-preload.js" \
-npx mockoon-cli start -d demo.json -p 3000
+#!/bin/sh
+
+# Function that (re)starts Mockoon with demo.json
+start_server() {
+  kill -9 $(lsof -ti :3000)
+  NODE_OPTIONS="--require $(pwd)/mockoon-hbs-preload.js" \
+  npx mockoon-cli start -d demo.json -p 3000 &
+  pid="$!"
+}
+
+# First launch
+start_server
+
+# Now watch demo.json; when it’s modified, kill/restart
+while inotifywait -e modify demo.json; do
+  echo "[watcher] demo.json changed; restarting Mockoon…"
+  kill "$pid"
+  start_server
+done
